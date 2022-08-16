ffe136b902f917fa222f5ec179e0f2fbb582e954
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-08-16 20:20:28 +0000

    Add product urls and basic view

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=25)
     slug = models.SlugField(max_length=25)
-    
+
     def __str__(self):
         return self.name
 
@@ -26,20 +26,9 @@ class Product(models.Model):
     product_code = models.CharField(max_length=50, null=True, blank=True)
     name = models.CharField(max_length=50)
     description = models.TextField()
-    scent = models.ForeignKey('Scent', null=True, blank=True, on_delete=models.SET_NULL)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-
-
-class Scent(models.Model):
-    """
-    Creates a scent model containing the scents the products come in
-    """
-
-    name = models.CharField(max_length=50)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE,
-                                null=True, blank=True)
