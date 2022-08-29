b87ec331fc2de26fa17b645c29656c55cbe40d7c
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-08-29 19:43:37 +0000

    Add add to cart functionality

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -1,7 +1,5 @@
 from decimal import Decimal
 from django.conf import settings
-from django.shortcuts import get_object_or_404
-from products.models import Product
 
 
 def cart_contents(request):
@@ -9,7 +7,6 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
-    cart = request.session.get('cart', {})
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
@@ -21,3 +18,5 @@ def cart_contents(request):
         'delivery': delivery,
         'grand_total': grand_total,
     }
+
+    return context
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def view_cart(request):
+
+def cart(request):
     """A view to see the contents of customer shopping cart """
-    
-    return render(request, 'shopping_cart/cart.html')
+
+    return render(request, 'shopping_cart/cart.html',)
+
+
+def add_to_cart(request, item_id):
+    """Add products to the shopping cart"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
