8b0994e9b65a344b239af816b41ee2cd789158f4
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-09-04 08:33:07 +0000

    Style all auth pages

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('philosophy/', views.philosophy, name='philosophy'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    """A view to return index page """
+    """A view to return index page"""
     
     return render(request, 'home/index.html')
+
+
+def philosophy(request):
+    """A view to return philosophy page"""
+
+    return render(request, 'home/philosophy.html')
