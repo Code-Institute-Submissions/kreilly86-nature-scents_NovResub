272d70d04fab5af05e44b4bf8e07ccea02e4375d
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-09-27 17:39:51 +0000

    Add custom 404 page to project

--- nature_scents/urls.py
+++ nature_scents/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
     path('cart/', include('shopping_cart.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+handler404 = 'nature_scents.views.handler404'
