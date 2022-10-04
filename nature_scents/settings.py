0ab7e8feb4754a1b8d40efd4abc441f99200a6e0
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-10-04 20:49:29 +0000

    change debug setting

--- nature_scents/settings.py
+++ nature_scents/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'True'
+DEBUG = os.environ.get('DEVELOPMENT')
 
 ALLOWED_HOSTS = ['nature-scents.herokuapp.com', 'localhost']
 
