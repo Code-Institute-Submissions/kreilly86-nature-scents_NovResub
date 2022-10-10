8ca006eae4290c0bbf5afdf663ec94c6eea383d4
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-10-10 19:13:52 +0000

    Uninstall whitenoise and remove settings

--- nature_scents/settings.py
+++ nature_scents/settings.py
@@ -57,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'nature_scents.urls'
@@ -154,6 +153,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
