30887a84896ecc0cc8ba28d952bf7281b3e92a92
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-10-10 17:41:39 +0000

    Change settings.py static file address back to original

--- nature_scents/settings.py
+++ nature_scents/settings.py
@@ -153,7 +153,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'nature-scents/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
