96e235d93f009af99d8d54509e89c725fecfdaa9
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-10-18 12:59:36 +0000

    Change max digits on product model price and migrate

--- products/models.py
+++ products/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     product_code = models.CharField(max_length=50, null=True, blank=True)
     name = models.CharField(max_length=50)
     description = models.TextField()
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
