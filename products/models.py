05f034a5e77b63a70a216c43d9254f1baa3f82c5
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-10-23 21:01:31 +0000

    Create Review model, urls, view and render on product_detail page

--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from user_profiles.models import UserProfile
 
 
 class Category(models.Model):
@@ -35,3 +37,20 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """
+    A model enabling users to review
+    products
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
--- products/views.py
+++ products/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.http import HttpResponse
 
-from .models import Product, Category
-from .forms import ProductForm
+from .models import Product, Category, Review
+from .forms import ProductForm, ReviewForm
+from user_profiles.models import UserProfile
 
 
 def all_products(request):
@@ -117,3 +119,26 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
+
+
+@login_required
+def submit_review(request, product_id):
+    if request.method == 'POST':
+        try:
+            reviews = Review.objects.get(user__id=request.user, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Yoour review has been updated.')
+            return redirect(reverse('submit_review'))
+
+        except Review.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = Review()
+                data.subject = form.cleaned_data['subject']
+                data.review = form.cleaned_data['review']
+                data.product_id = product_id
+                data.user_id = request.user
+                data.save()
+                messages.success(request, 'Thank you! Your review has been submitted.')
+                return redirect(reverse('submit_review'))
