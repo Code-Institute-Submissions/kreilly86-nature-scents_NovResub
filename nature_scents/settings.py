bce5603a35fbe17ac8b3941ebdf73a9b4696ff40
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-11-26 14:35:49 +0000

    Update Product model to include Cloudinary field

--- nature_scents/settings.py
+++ nature_scents/settings.py
@@ -152,7 +152,6 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media'),]
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from user_profiles.models import UserProfile
+from cloudinary.models import CloudinaryField
 
 
 class Category(models.Model):
@@ -33,7 +34,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = CloudinaryField(null=True, blank=True)
 
     def __str__(self):
         return self.name
